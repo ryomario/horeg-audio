052721d692ce29f6eb0b464bb35d48c53183823e
Mario <ardamomo3@gmail.com>
2026-09-06 01:54:45 +0700

    fix(chore): library constructor error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { HoregAudio } from './Player';
 
+// Attach self references for seamless interop across ESM, CJS, and browser globals
+(HoregAudio as any).HoregAudio = HoregAudio;
+(HoregAudio as any).default = HoregAudio;
+
 export * from './types';
 export { HoregAudio };
 export default HoregAudio;
